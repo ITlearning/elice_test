52bad6ebfbb3b3bd1a958aa14fc42049a720c3c1
Tabber <tabber@jejememe.com>
2023-11-26 02:23:26 +0900

    Initial Commit

---
+++ elise_test/elise_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  elise_testApp.swift
+//  elise_test
+//
+//  Created by Tabber on 2023/11/26.
+//
+
+import SwiftUI
+
+@main
+struct elise_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
